63d696fadb185ddd6947554284c59e0f5013527f
tinion <cmfrydos@gmail.com>
2024-09-03 18:51:40 +0200

    made it work (with & without cache)

--- babelTransform.js
+++ babelTransform.js
@@ -5,15 +5,14 @@ module.exports = {
     const alias = options?.config?.moduleNameMapper;
     const extensions = options.config.moduleFileExtensions;
     const babelTransformer = babelJest.createTransformer({
-     presets: [
-        ["@babel/preset-react", { runtime: "automatic" }], // Ensuring JSX transform
-        ["@babel/preset-env"],
-        ["@babel/preset-typescript", { isTSX: true, allExtensions: true }] // Adjusted for TSX
-      ],
-      plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
+      presets: [["@babel/preset-react"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
+      plugins: [
+        ["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions, isCacheEnabled: false }],
+      ], // set to true when all else worked
       babelrc: false,
       configFile: false,
     });
+
     return babelTransformer.process(sourceText, sourcePath, options);
   },
 };
