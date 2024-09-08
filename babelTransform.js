6019a3af22e7c419ec38a0c9ca9cdaeb5c1b6e80
tinion <cmfrydos@gmail.com>
2024-09-08 14:15:08 +0200

    babelTransform.js: remove cache comment

--- babelTransform.js
+++ babelTransform.js
@@ -6,9 +6,7 @@ module.exports = {
     const extensions = options.config.moduleFileExtensions;
     const babelTransformer = babelJest.createTransformer({
       presets: [["@babel/preset-react"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
-      plugins: [
-        ["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions, isCacheEnabled: false }],
-      ], // set to true when all else worked
+      plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
       babelrc: false,
       configFile: false,
     });
